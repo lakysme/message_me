6b12c8810178f8712a51d8710b8ca724ae8885b0
Lakysme Rocha <lakysme@gmail.com>
2023-05-24 17:43:45 +0100

    Build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: "John", password: "password")
+User.create(username: "Mary", password: "password")
+User.create(username: "Luke", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Evgeny", password: "password")
