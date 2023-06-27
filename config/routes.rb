40cf1702be986eabdd1b819d760f852f17d54ca3
Lakysme Rocha <lakysme@gmail.com>
2023-06-27 18:06:20 +0100

    Add authentication system - create and destroy sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 end
